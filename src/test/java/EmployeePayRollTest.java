7c330d974daf53ae17064f4b743209352849cc47
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-11 01:26:33 +0530

    Added to print from file.

--- src/test/java/EmployeePayRollTest.java
+++ src/test/java/EmployeePayRollTest.java
@@ -10,23 +10,11 @@ public class EmployeePayRollTest {
         empPayRoll.add(new EmployeePayRoll(2,"Ashvini",500000));
         EmployeePayRollService empPayRollService = new EmployeePayRollService(empPayRoll);
         empPayRollService.writeData("File");
+        empPayRollService.printData("File");
         int entries = empPayRollService.noOfEntries("File");
         boolean result = entries==2 ? true : false;
         Assert.assertTrue(result);
     }
-
-@Test
-public void given3EmployeesStoreToFileShouldPassTest()
-{
-    ArrayList<EmployeePayRoll> empPayRoll = new ArrayList<EmployeePayRoll>();
-    empPayRoll.add(new EmployeePayRoll(1,"Avi",1000000));
-    empPayRoll.add(new EmployeePayRoll(2,"Ashvini",500000));
-    EmployeePayRollService empPayRollService = new EmployeePayRollService(empPayRoll);
-    empPayRollService.writeData("File");
-    int entries = empPayRollService.noOfEntries("File");
-    boolean result = entries==2 ? true : false;
-    Assert.assertTrue(result);
-}
 
 }
 
