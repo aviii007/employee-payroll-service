58101c18297fc7cc575e44499702bd07cf8e7208
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-11 01:06:37 +0530

    Added the read and write to console.

--- src/main/java/EmployeePayRollService.java
+++ src/main/java/EmployeePayRollService.java
@@ -1,5 +1,39 @@
+import java.util.*;
+
 public class EmployeePayRollService {
-    public static void main(String[] args) {
-        System.out.println("welcome to employee payroll service");
+    private static List<EmployeePayRoll> empPayRollList;
+    private static Scanner sc = new Scanner(System.in);
+
+    public EmployeePayRollService(List<EmployeePayRoll> empPayRollList) {
+        this.empPayRollList = empPayRollList;
     }
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayRoll> empPayRollList = new ArrayList<EmployeePayRoll>();
+        EmployeePayRollService empPayRollService = new EmployeePayRollService(empPayRollList);
+
+        empPayRollService.readData();
+
+        empPayRollService.writeData();
+
+    }
+
+    private static void readData() {
+        System.out.println("Please Enter the following details :");
+        System.out.println("Enter ID");
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter Name :");
+        String name = sc.nextLine();
+        System.out.println("Enter Salary :");
+        double salary = sc.nextDouble();
+
+        EmployeePayRoll empPayRollObject = new EmployeePayRoll(id, name, salary);
+        empPayRollList.add(empPayRollObject);
+    }
+
+    private void writeData() {
+        System.out.println("Employee Pay Roll Data : \n" + empPayRollList.get(0).toString());
+    }
+
 }
